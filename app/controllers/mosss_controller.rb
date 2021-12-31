04443f51d6cc1a8a947a7e0b54c6930c95b13c39
goto0214 <yuusuke0214goto1997@outlook.jp>
2021-12-31 16:04:11 +0000

    [update]

--- app/controllers/mosss_controller.rb
+++ app/controllers/mosss_controller.rb
@@ -2,18 +2,25 @@ class MosssController < ApplicationController
 
 
   def new
+    @moss = Moss.new
   end
 
   def create
+    @moss = Moss.new(moss_params)
+    @moss.save
+    redirect_to mosss_path
   end
 
   def index
+    @mosss = Moss.all
   end
 
   def show
+    @moss = Moss.find(params[:id])
   end
 
   def edit
+    @moss = Moss.find(params[:id])
   end
 
   def update
