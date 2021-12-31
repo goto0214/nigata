b1229bb02b113e9b88ff51867a696b5f48172b60
goto0214 <yuusuke0214goto1997@outlook.jp>
2021-12-31 15:54:59 +0000

    [update]

--- app/controllers/mosss_controller.rb
+++ app/controllers/mosss_controller.rb
@@ -1,2 +1,26 @@
 class MosssController < ApplicationController
+
+
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
 end
